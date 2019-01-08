2974f5528759e2896f229f94c6d102d7ff412628
Santiago <santiagojmontero@outlook.com>
2019-01-07 22:38:54 -0430

    some little changes with the search bar and styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
 import React, { Component } from 'react';
+import SearchField from 'react-search-field';
 import './App.css';
 
-// Empezare a configurar el repositorio git con esto
+//Prueba
+const testArray = 
+[
+  "Gallo verde",
+  "Gral. Rafael Urdaneta",
+  "La bandera"
+];
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-       
+       searchedValue: ''
     }
   }
   
+  onChange = (value) => {
+    this.setState({searchedValue: value});
+  }
+
   render() {
     return (
-      <div className="App body-content row">
-        <div className="four columns">
-          <span>2</span>
-          <span>2</span>
+      <div className="App">
+        {/* HEADER */}
+        <div className="header">
+          
         </div>
-        <div className="eight columns">
-          <span>8</span>
+        <div className="row body-content">
+          <div className="four columns">
+            <div className="row">
+              <div className="twelve columns">
+                <h3>NOMBRE DE LA RUTA</h3>
+              </div>
+            </div>
+            <div className="row">
+              <div className="twelve columns">
+              <div className="padded align-center">
+                <SearchField 
+                    placeholder='Search item'
+                    onChange={this.onChange}
+                  />
+              </div>
+              </div>
+            </div>
+            <div className="row">
+              {/* Elements container */}
+              <div className="twelve columns">
+                {/* Array map() */}
+                {
+                  testArray.map((val, i)=>{
+                    const isFound = val.toLowerCase().includes(this.state.searchedValue.toLowerCase());
+                    return (
+                      this.state.searchedValue==='' || isFound ?
+                      <div key={i} className="row station-list-element">
+                        <div className="eight columns">
+                          <span>{val}</span>
+                        </div>
+                      </div>
+                      :
+                      <div key={i}></div>
+                    );
+                  })
+                }
+              </div>
+            </div>
+          </div>
+          <div className="eight columns">
+            <span>8</span>
+          </div>
         </div>
       </div>
     );
