4d71ac29b36c2157a1e3dc2cb9fa027ef1453c0b
Santiago <santiagojmontero@outlook.com>
2019-01-07 19:35:59 -0430

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// Empezare a configurar el repositorio git con esto
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+       
+    }
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App body-content row">
+        <div className="four columns">
+          <span>2</span>
+          <span>2</span>
+        </div>
+        <div className="eight columns">
+          <span>8</span>
+        </div>
       </div>
     );
   }
