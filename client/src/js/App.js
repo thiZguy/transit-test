1056a6a8b411ff6f7f117cf83595f54909fb2ea1
Santiago <santiagojmontero@outlook.com>
2019-01-15 18:11:11 -0430

    modified repo to have server code

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import SearchField from 'react-search-field';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import GoogleMapReact from 'google-map-react';
+
 import '../css/app.css';
 import '../css/tabs.css';
 
+// CLAVE: AIzaSyD91ubThsz5ZvUNqZhkhl2_vHkL7miQ6xo
+const AnyReactComponent = ({ text }) => <div style={{ borderWidth: '0px 0px 1px 0px',
+borderColor: '#545a5a'}}>{text}</div>;
+
+
 const defaultStations = [
-  "Gallo verde",
-  "Gral. Rafael Urdaneta",
-  "La bandera"
+  {text: "Gallo verde", lat: -33.456, long: -70.674211 },
+  {text: "Gral. Rafael Urdaneta", lat: -33.459, long: -70.674211 },
+  {text: "La bandera", lat: -33.453, long: -70.674211 }
 ]
 
 class App extends Component {
+  static defaultProps = {
+    center: {
+      lat: -33.44,
+      lng: -70.669266
+    },
+    zoom: 11
+  };
+
   constructor(props) {
     super(props)
     this.state = {
@@ -36,12 +51,12 @@ class App extends Component {
   renderStations = (stations, searchedValue) => 
    <div>
       {
-        stations.map((val, i) => {
-          const isFound = val.toLowerCase().includes(searchedValue.toLowerCase());
+        stations.map((element, i) => {
+          const isFound = element.text.toLowerCase().includes(searchedValue.toLowerCase());
           return (
             searchedValue === '' || isFound ?
               <div key={i} className="row station-list-element">
-                <span>{val}</span>
+                <span>{element.text}</span>
               </div>
               :
               <div key={i}></div>
@@ -62,7 +77,7 @@ class App extends Component {
 
         </div>
         <div className="row body-content">
-          <div className="four columns">
+          <div className="four columns flexbox-style">
             <div className="row">
               <h3>NOMBRE DE LA RUTA</h3>
             </div>
@@ -72,44 +87,44 @@ class App extends Component {
                     placeholder='Buscar ruta...'
                     onChange={newSearchval}
                     classNames="shadow"
-                  />
+                />
               </div>
             </div>
             <div className="row padded">
-              <Tabs onSelect={(index) => this.changeTab(index)}>
+              <Tabs
+                onSelect={(index) => this.changeTab(index)}
+                selectedTabClassName="selected-tab"
+                // selectedTabPanelClassName="selected-tab-panel"
+              >
                 <TabList>
                   <Tab>Recorridos</Tab>
                   <Tab>Favoritos</Tab>
                 </TabList>
                 <TabPanel>
-                  {() => this.renderStations(stations,searchedValue)}
+                  {this.renderStations(stations,searchedValue)}
                 </TabPanel>
                 <TabPanel>
-                  {() => this.renderStations(stations,searchedValue)}
+                  {this.renderStations(stations,searchedValue)}
                 </TabPanel>
               </Tabs>
-            </div>
-            <div className="row">
-              {/* stations container */}
-              {
-                stations.map((val, i)=>{
-                  const isFound = val.toLowerCase().includes(searchedValue.toLowerCase());
-                  return (
-                    searchedValue==='' || isFound ?
-                    <div key={i} className="row station-list-element">
-                      <span>{val}</span>
-                    </div>
-                    :
-                    <div key={i}></div>
-                  );
-                })
-              }
             </div>
           </div>
           <div className="eight columns flexbox-style">
             <div className="row">
-              <h3>8</h3>
-            </div>      
+              <div style={{ height: '250px', width: '500px' }}>
+                <GoogleMapReact
+                  bootstrapURLKeys={{ key: 'AIzaSyD91ubThsz5ZvUNqZhkhl2_vHkL7miQ6xo' }}
+                  defaultCenter={this.props.center}
+                  defaultZoom={this.props.zoom}
+                >
+                  <AnyReactComponent
+                    lat={-33.455548}
+                    lng={-70.630209}
+                    text="AAAAAAAAAAA"
+                  />
+                </GoogleMapReact>
+              </div>
+            </div>
           </div>
         </div>
       </div>
